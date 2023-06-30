095ae1ee313c0d41c0cb047cdb95f8aadc93bcdf
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-06-30 11:21:56 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -465,25 +465,25 @@ fig.text(
 # add text
 fig.text(
     1.2, 0.417, roles[1] + "\nPercentile Rank" ,size=19,
-    ha="center", fontweight='bold', fontfamily='Consolas', color="black"
+    ha="center", fontweight='bold', fontfamily='Consolass', color="black"
 )
 
 fig.text(
     1.15, 0.267, score3, size=50,
-    ha="left", fontweight='bold', fontfamily='Consolas', color="black",
+    ha="left", fontweight='bold', fontfamily='Consolass', color="black",
     bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.3', lw=3)
 )
 
 # add text
 fig.text(
     1.2, 0.166, roles[2] +"\nPercentile Rank" ,size=19,
-    ha="center", fontweight='bold', fontfamily='Consolas', color="black"
+    ha="center", fontweight='bold', fontfamily='Consolass', color="black"
 )
 
 # add text
 fig.text(
     0.745, 0.1, "Note: Top 5 European Leagues Players with 1080+ Minutes Included | Data: Opta | By @chunhang7" ,
-    size=15, ha="center", fontweight='bold', fontfamily='Consolas', color="black"
+    size=15, ha="center", fontweight='bold', fontfamily='Consolass', color="black"
 )
 
 # Display the plot

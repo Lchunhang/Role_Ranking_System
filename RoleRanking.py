2c4d3026178991e69e2440439c0244ab8991c8c8
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-06-30 14:47:36 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -305,7 +305,6 @@ def CF_Rating(df):
 
 if not df.empty and 'Position' in df.columns and len(df['Position']) > 0:
     if df['Position'].iloc[0] == 'Centre-Back':
-        @st.cache 
         df = CB_Rating(df)
         df = pd.merge(df, age, on="Player")
         df.rename(columns = {'Minutes Played_y':'Minutes Played'},inplace = True)

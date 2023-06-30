a5cd2482d396f7a2491617976e6719de83e5397a
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-06-30 09:49:18 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -98,13 +98,13 @@ df['Shot-Ending Carries per Touch'] = df['Shot-Ending Carries'] / df['Live Touch
 #######################################################################
 
 with st.sidebar:
-    st.markdown('<h1 style="font-family: Consolas; src: url('fonts/Consolas.ttf'); font-size: 34px;">Select Your Players Here...</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="font-size: 34px;">Select Your Players Here...</h1>', unsafe_allow_html=True)
     options = df["Player"].dropna().tolist()
     player = st.selectbox('Player', options)
     position = df.loc[df['Player'] == player, 'Position'].values[0]
     df = df.loc[(df['Position'] == position)].reset_index(drop= True)
     st.write(position+" Template")
-    st.markdown('<h1 style="font-family: Consolas; font-size: 34px;">..And Let The Magic Happen ➡️</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="font-size: 34px;">..And Let The Magic Happen ➡️</h1>', unsafe_allow_html=True)
     
 
 #Extract age, ready to merge

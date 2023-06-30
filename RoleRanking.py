4041b8cd342e193a6de3195e3bce5df54bbd6930
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-06-30 10:10:09 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -18,7 +18,7 @@ from streamlit import components
 
 hide_github_icon = """
 <style>
-.css-1jc7ptx{ display: none; }
+.css-1jc7ptx, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
 </style>
 """
 st.markdown(hide_github_icon,unsafe_allow_html=True)

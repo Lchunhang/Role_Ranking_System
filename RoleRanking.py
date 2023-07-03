1652ce0148577d908dc390544fdbdcf8160134c3
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-07-03 08:42:40 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -108,14 +108,30 @@ df['Shot-Ending Carries per Touch'] = df['Shot-Ending Carries'] / df['Live Touch
 #######################################################################
 
 with st.sidebar:
-    st.markdown('<h1 style="font-size: 34px;">Select Your Players Here...</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="font-family: Consolas; font-size: 34px;">Select Your Players Here...</h1>', unsafe_allow_html=True)
     options = df["Player"].dropna().tolist()
     player = st.selectbox('Player', options)
-    position = df.loc[df['Player'] == player, 'Position'].values[0]
-    df = df.loc[(df['Position'] == position)].reset_index(drop= True)
-    st.write(position+" Template")
-    st.markdown('<h1 style="font-size: 34px;">..And Let The Magic Happen ➡️</h1>', unsafe_allow_html=True)
     
+    options2 = df["Position"].dropna().unique().tolist()
+    default_position = df.loc[df['Player'] == player, 'Position'].values[0]
+    user_input = st.selectbox("Enter Position Template (optional)", [default_position] + options2)
+
+    if user_input != default_position:
+        # Filter based on user input
+        position = user_input
+    else:
+        # Default behavior with 'Position' derived from 'Player' column
+        position = default_position
+
+    # If the user has selected a position, update the position of the selected player
+    if user_input != default_position:
+        df.loc[df['Player'] == player, 'Position'] = position
+
+    # Filter the dataframe based on the position
+    df = df.loc[df['Position'] == position].reset_index(drop=True)
+    st.write(position+" Template")
+    st.markdown('<h1 style="font-family: Consolas; font-size: 34px;">..And Let The Magic Happen ➡️</h1>', unsafe_allow_html=True)
+
 
 #Extract age, ready to merge
 age = df[['Player','True Age', 'Minutes Played']]

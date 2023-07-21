6bd4117762e7114e9a50d9549e82b69786122096
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-07-22 01:27:25 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -170,7 +170,7 @@ scaler = StandardScaler()
 normalized_data = scaler.fit_transform(sdf[sdf.columns[4:]])
 
 # Apply K-means clustering
-num_clusters = 3  # You can choose the number of clusters based on your requirements
+num_clusters = 30  # You can choose the number of clusters based on your requirements
 kmeans = KMeans(n_clusters=num_clusters, n_init='auto', random_state=42)
 clusters = kmeans.fit_predict(normalized_data)
 

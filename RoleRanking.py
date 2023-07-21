bedd0000ab35728c3edc6027ff179bc5c222cf34
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-07-22 01:48:30 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -21,20 +21,10 @@ from sklearn.preprocessing import StandardScaler
 
 hide_github_icon = """
 <style>
-.css-1wbqy5l.ejj6ze1 { display: none; } 
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
 </style>
 """
 st.markdown(hide_github_icon,unsafe_allow_html=True)
-
-hide_streamlit_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style>
-
-"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
 
 
 #Remove Warnings

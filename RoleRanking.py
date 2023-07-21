f68790fccd1c99b7713d380d1862173d21352903
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-07-22 01:43:49 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -26,7 +26,13 @@ hide_github_icon = """
 """
 st.markdown(hide_github_icon,unsafe_allow_html=True)
 
+hide_streamlit_style = “”"
 
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+
+“”"
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 #Remove Warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 pd.options.mode.chained_assignment = None 

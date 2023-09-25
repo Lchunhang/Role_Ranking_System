c8cd7474b8364b185c63eb4066c85b284a96debd
Lchunhang <99263192+Lchunhang@users.noreply.github.com>
2023-09-25 15:35:52 +0800

    Update RoleRanking.py

--- RoleRanking.py
+++ RoleRanking.py
@@ -49,7 +49,7 @@ with st.expander("App Details"):
     The Role Ranking System assigns varrying weightages to different metrics based on their relevance to specific roles, reflecting the author's perspective backed by extensive research.\n 
     Similarity Function is based on K-Means Clustering, to identify similar players based on playstyle.\n
     
-    Note: Only Outfielders from Top 7 Leagues with over 1080 Minutes Played in 2022/23 Season are Included for Selection.88
+    Note: Only Outfielders from Top 777 Leagues with over 1080 Minutes Played in 2022/23 Season are Included for Selection.
     ''')
         
 # Create a Streamlit button to clear the cache
